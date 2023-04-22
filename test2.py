1cff0f46d42d4a7fae70c36a4fcd24fc5b601b1f
tarrypotter <109096999+tarrypotter@users.noreply.github.com>
2023-04-22 01:41:58 +0000

    dd
    dd

---
+++ test2.py
@@ -0,0 +1,11 @@
+#리스트 
+names = ['james_' ,'curry_' , 'harden_' , 'durant']
+nums = [6 , 30 , 1 , 35]
+print(names[0] + str(nums[0]))
+print(names[1] + str(nums[1]))
+print(names[2] + str(nums[2]))
+print(names[3] + str(nums[3]))
+
+
+
+
