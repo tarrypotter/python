c4dc24699a12cbb8c651fada3b06912ede52f456
tarrypotter <109096999+tarrypotter@users.noreply.github.com>
2023-04-22 01:38:52 +0000

    dd

---
+++ test.py
@@ -0,0 +1,22 @@
+#한줄 주석
+
+#리스트 
+names = ['james_' ,'curry_' , 'harden_']
+nums = [6 , 30 ,1]
+print(names[0] + str(nums[0]))
+print(names[1] + str(nums[1]))
+print(names[2] + str(nums[2]))
+
+#전체 주석
+"""
+
+names = ['james_' ,'curry_' , 'harden_']
+nums = [6 , 30 ,1]
+print(names[0] + str(nums[0]))
+print(names[1] + str(nums[1]))
+print(names[2] + str(nums[2]))
+
+"""
+
+
+
